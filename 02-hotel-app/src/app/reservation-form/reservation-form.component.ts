47c453fac6e5916c592a11c1d9baf53780b3fb3e
Debarghyya <debarghyya@gmail.com>
2025-11-06 18:20:22 +0530

    Added reactive form validation and validation messages

--- 02-hotel-app/src/app/reservation-form/reservation-form.component.ts
+++ 02-hotel-app/src/app/reservation-form/reservation-form.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css']
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit {
+
+  reservationForm: FormGroup = new FormGroup({});
+
+  // Initialize FormBuilder using Dependency Injection when the instance of this component is created, even before ngOnInit (app initialization) is called
+  constructor(private formBuilder: FormBuilder) {}
+
+  // Add validation to the form elements on app initialization, hence added on ngOnInit
+  // The key names of the formBuilder.group has to exactly match the formControlName in the HTML template so that Angular can link them
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if(this.reservationForm.valid) {
+      console.log('Valid');
+    }
+  }
 
 }
