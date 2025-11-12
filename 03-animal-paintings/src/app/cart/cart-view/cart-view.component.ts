0a2dc552ea36ae2b8e9a82c44d798ab846bcf862
Debarghyya <debarghyya@gmail.com>
2025-11-12 17:22:26 +0530

    Added cart styling and clear mock functionality

--- 03-animal-paintings/src/app/cart/cart-view/cart-view.component.ts
+++ 03-animal-paintings/src/app/cart/cart-view/cart-view.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart-view',
   templateUrl: './cart-view.component.html',
   styleUrls: ['./cart-view.component.css']
 })
-export class CartViewComponent {
+export class CartViewComponent implements OnInit{
 
+  cartItems: Product[] = [];
+  totalPrice: number = 0;
+  
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartService.getCartItems().subscribe(data => {
+      this.cartItems = data;
+      // Calculate total price after cartItems is populated in subscribe
+      this.totalPrice = this.getTotalPrice();
+    })
+  }
+
+  getTotalPrice(): number {
+    let total = 0;
+    for(let item of this.cartItems) {
+      total += item.price;
+    }
+    return total;
+  }
+
+  clearCart(): void {
+    this.cartService.clearCart().subscribe(() => {
+      console.log("Cart cleared successfully");
+    });
+  }
 }
