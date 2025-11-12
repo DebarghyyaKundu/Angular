ba752630eb2d3fb9b4cfa8073f493f5093365ca3
Debarghyya <debarghyya@gmail.com>
2025-11-12 17:33:55 +0530

    Added toasters

--- 03-animal-paintings/src/app/product/product-list/product-list.component.ts
+++ 03-animal-paintings/src/app/product/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from 'src/app/model/product';
+import { CartService } from 'src/app/cart/cart.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-product-list',
@@ -11,11 +13,29 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe(res => {
       this.products = res;
+    });
+  }
+
+  // When a product is clicked on, add it to the cart
+  addToCart(product: Product): void {
+    this.cartService.addToCart(product).subscribe({
+      // When the product is added to cart then next is executed
+      next: () => {
+        this.snackBar.open("Product added to cart!", "", {
+          duration: 2000,
+          horizontalPosition: "right",
+          verticalPosition: "top"
+        });
+      }
     });
   }
 }
