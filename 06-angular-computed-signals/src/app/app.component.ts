67fac675da95465c7607f81fd791b7e84a6619ed
Debarghyya <debarghyya@gmail.com>
2025-11-14 18:04:36 +0530

    Added signal changes

--- 06-angular-computed-signals/src/app/app.component.ts
+++ 06-angular-computed-signals/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = '06-angular-computed-signals';
+
+  /**
+   * Computed signal is a signal that is derived from other signals. It automatically updates when any of its dependencies change.
+   * In this example, totalPrice is a computed signal that depends on price and quantity.
+   * Whenever quantity changes, totalPrice will automatically recalculate.
+   */
+
+  price = 19;
+  quantity = signal(10);
+
+  // totalPrice is a read-only computed signal since it cannot be changed directly. It should only be changed by updating quantity signal.
+  totalPrice = computed(() => this.price * this.quantity());
+
+  changeQuantity(event: Event) {
+    this.quantity.set((event.target as HTMLInputElement).valueAsNumber);
+  }
 }
